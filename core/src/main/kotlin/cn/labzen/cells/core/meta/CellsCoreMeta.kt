cc6962502ff9a72a804025ff049ef641b5da4d6b
Dean Zhao <rcarlosdasilva@qq.com>
2022-07-08 10:23:41 +0800

    feat: core, 添加meta信息

---
+++ core/src/main/kotlin/cn/labzen/cells/core/meta/CellsCoreMeta.kt
@@ -0,0 +1,9 @@
+package cn.labzen.cells.core.meta
+
+import cn.labzen.meta.LabzenMeta
+
+class CellsCoreMeta : LabzenMeta {
+
+  override fun description(): String =
+    "核心包，定义最基本的包依赖集合，并提供普适基础工具与底层功能"
+}
