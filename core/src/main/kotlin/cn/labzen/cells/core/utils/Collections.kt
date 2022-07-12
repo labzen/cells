873619ec33f6b4a259c2dc5c71458edb8417f589
Dean Zhao <rcarlosdasilva@qq.com>
2022-07-12 14:24:45 +0800

    feat: core, 更新Collections

--- core/src/main/kotlin/cn/labzen/cells/core/utils/Collections.kt
+++ core/src/main/kotlin/cn/labzen/cells/core/utils/Collections.kt
@@ -1,6 +1,5 @@
 package cn.labzen.cells.core.utils
 
-import org.checkerframework.checker.units.qual.K
 import java.util.Collections
 
 object Collections {
@@ -9,7 +8,7 @@ object Collections {
    * 集合是NULL或0元素
    */
   @JvmStatic
-  fun isNullOrEmpty(collection: Iterable<*>?) =
+  fun isNullOrEmpty(collection: Collection<*>?) =
     collection?.toList()?.isEmpty() ?: true
 
   /**
@@ -18,8 +17,18 @@ object Collections {
    * @return List<E> 剔除null后的集合，可能是空集合，但不会为null
    */
   @JvmStatic
+  @Deprecated("定位不是很准确", ReplaceWith("this.removeNullElement(List)"))
   fun <E : Any> removeNull(collection: Iterable<E?>?): List<E> =
     collection?.filterNotNull() ?: Collections.emptyList()
+
+  /**
+   * 剔除集合中的null元素
+   * @param list List<E> 集合
+   * @return List<E> 剔除null后的集合，可能是空集合，但不会为null
+   */
+  @JvmStatic
+  fun <E : Any> removeNullElement(list: List<E?>): List<E> =
+    list.filterNotNull()
 
   /**
    * 剔除集合中的null值与空内容字符串，如果参数为空，返回空集合
@@ -27,10 +36,21 @@ object Collections {
    * @return List<E> 剔除后的集合，可能是空集合，但不会为null
    */
   @JvmStatic
+  @Deprecated("定位不是很准确", ReplaceWith("this.removeBlankElement(List)"))
   fun removeBlank(collection: Iterable<String?>?): List<String> =
     collection?.filterNotNull()?.filterNot { it.isBlank() } ?: Collections.emptyList()
 
+  /**
+   * 剔除集合中的null值与空内容字符串
+   * @param list List<String> 集合
+   * @return List<String> 剔除后的集合，可能是空集合，但不会为null
+   */
   @JvmStatic
+  fun removeBlankElement(list: List<String?>): List<String> =
+    list.filterNotNull().filterNot { it.isBlank() }
+
+  @JvmStatic
+  @Deprecated("定位不是很准确", ReplaceWith("this.allSame(Iterable, block)"))
   fun <E : Any, K> allEquals(collection: Iterable<E?>?, selector: (E) -> K): Boolean {
     collection ?: return false
 
@@ -38,4 +58,47 @@ object Collections {
       if (it == null) null else selector(it)
     }.toSet().size == 1
   }
+
+  /**
+   * 如果集合中所有的元素相同，返回true
+   * @param collection Iterable<E> 集合
+   * @param selector 选择器，挑选判断的目标
+   * @return Boolean 如果所有元素全部相同
+   */
+  @JvmStatic
+  fun <E : Any, K> allSame(collection: Collection<E?>, selector: ((E) -> K)? = null): Boolean =
+    collection.filterNotNull().map {
+      if (selector == null) {
+        it
+      } else {
+        selector(it)
+      }
+    }.toSet().size <= 1
+
+  @JvmStatic
+  fun <C : Collection<E?>, E : Any> insureContains(collection: C, element: E): C =
+    @Suppress("UNCHECKED_CAST")
+    if (collection.contains(element)) {
+      collection
+    } else if (collection is List<*>) {
+      collection.toMutableList().also {
+        it.add(element)
+      } as C
+    } else {
+      collection.toMutableSet().also {
+        it.add(element)
+      } as C
+    }
+
+  @JvmStatic
+  fun <C : Collection<E?>, E : Any> onValueAtLeast(collection: C, element: E): C =
+    collection.ifEmpty {
+      @Suppress("UNCHECKED_CAST")
+      if (collection is List<*>) {
+        collection.toMutableList().also { it.add(element) } as C
+      } else {
+        collection.toMutableSet().also { it.add(element) } as C
+      }
+    }
+
 }
--- core/src/test/java/cn/labzen/cells/core/utils/CollectionsTest.java
+++ core/src/test/java/cn/labzen/cells/core/utils/CollectionsTest.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static cn.labzen.cells.core.utils.Collections.*;
@@ -23,7 +24,7 @@ class CollectionsTest {
   void testRemoveNull() {
     List<String> source = Lists.newArrayList("1", "", "2", null, "3");
     List<String> expected = Lists.newArrayList("1", "", "2", "3");
-    List<String> target = removeNull(source);
+    List<String> target = removeNullElement(source);
     assertNotNull(target);
     assertIterableEquals(expected, target);
   }
@@ -32,7 +33,7 @@ class CollectionsTest {
   void testRemoveBlank() {
     List<String> source = Lists.newArrayList("1", "  ", "2", null, "3");
     List<String> expected = Lists.newArrayList("1", "2", "3");
-    List<String> target = removeBlank(source);
+    List<String> target = removeBlankElement(source);
     assertNotNull(target);
     assertIterableEquals(expected, target);
   }
@@ -44,8 +45,34 @@ class CollectionsTest {
     collection.add(new SimpleBean("b", 2, true));
     collection.add(new SimpleBean("c", 3, true));
 
-    Assertions.assertTrue(allEquals(collection, SimpleBean::getBooleanValue));
-    Assertions.assertFalse(allEquals(collection, SimpleBean::getStringValue));
+    //Assertions.assertTrue(allEquals(collection, SimpleBean::getBooleanValue));
+    //Assertions.assertFalse(allEquals(collection, SimpleBean::getStringValue));
+    Assertions.assertTrue(allSame(collection, SimpleBean::getBooleanValue));
+    Assertions.assertFalse(allSame(collection, SimpleBean::getStringValue));
   }
 
+  @Test
+  void testInsureContains() {
+    ArrayList<String> strings = Lists.newArrayList("a", "b", "c");
+    String word = "x";
+
+    strings = insureContains(strings, word);
+    Assertions.assertEquals(4, strings.size());
+    Assertions.assertTrue(strings.contains(word));
+
+    strings = insureContains(strings, word);
+    Assertions.assertEquals(4, strings.size());
+  }
+
+  @Test
+  void testOnValueAtLeast() {
+    ArrayList<String> strings = Lists.newArrayList();
+    String word = "x";
+
+    strings = onValueAtLeast(strings, "x");
+    Assertions.assertEquals(1, strings.size());
+
+    strings = onValueAtLeast(strings, "x");
+    Assertions.assertEquals(1, strings.size());
+  }
 }
