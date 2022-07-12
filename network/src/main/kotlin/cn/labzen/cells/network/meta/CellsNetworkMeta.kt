05f892700beb504b76687c6a9ee600c7de403f49
Dean Zhao <rcarlosdasilva@qq.com>
2022-07-12 15:07:46 +0800

    feat: network, 添加meta信息

---
+++ network/src/main/kotlin/cn/labzen/cells/network/meta/CellsNetworkMeta.kt
@@ -0,0 +1,9 @@
+package cn.labzen.cells.network.meta
+
+import cn.labzen.meta.LabzenMeta
+
+class CellsNetworkMeta : LabzenMeta {
+
+  override fun description(): String =
+    "网络包，提供轻量级的Http/Tcp服务器或客户端快速开发，以及快捷的网络功能支撑"
+}
