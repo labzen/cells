18b46f8ed4338c5c9405cf686cb374b7f0e17863
Dean Zhao <rcarlosdasilva@qq.com>
2022-07-18 09:30:31 +0800

    refactor: core, meta更新1.1版本，修改原spi接口

--- core/src/main/kotlin/cn/labzen/cells/core/meta/CellsCoreMeta.kt
+++ core/src/main/kotlin/cn/labzen/cells/core/meta/CellsCoreMeta.kt
@@ -1,8 +1,14 @@
 package cn.labzen.cells.core.meta
 
-import cn.labzen.meta.LabzenMeta
+import cn.labzen.meta.component.LabzenComponent
 
-class CellsCoreMeta : LabzenMeta {
+class CellsCoreMeta : LabzenComponent {
+
+  override fun mark(): String =
+    "Labzen-Cells.Core"
+
+  override fun packageBased(): String =
+    "cn.labzen.cells.core"
 
   override fun description(): String =
     "核心包，定义最基本的包依赖集合，并提供普适基础工具与底层功能"
