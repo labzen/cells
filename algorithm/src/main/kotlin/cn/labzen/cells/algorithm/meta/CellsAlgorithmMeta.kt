c48e411f0b5e03e379e103d3cffb84171ad99ab4
Dean Zhao <rcarlosdasilva@qq.com>
2022-07-18 11:20:16 +0800

    refactor: algorithm, 修改原spi接口

--- algorithm/src/main/kotlin/cn/labzen/cells/algorithm/meta/CellsAlgorithmMeta.kt
+++ algorithm/src/main/kotlin/cn/labzen/cells/algorithm/meta/CellsAlgorithmMeta.kt
@@ -1,8 +1,14 @@
 package cn.labzen.cells.algorithm.meta
 
-import cn.labzen.meta.LabzenMeta
+import cn.labzen.meta.component.LabzenComponent
 
-class CellsAlgorithmMeta : LabzenMeta {
+class CellsAlgorithmMeta : LabzenComponent {
+
+  override fun mark(): String =
+    "Labzen-Cells.Algorithm"
+
+  override fun packageBased(): String =
+    "cn.labzen.cells.algorithm"
 
   override fun description(): String =
     "算法包，提供常用的算法功能封装，例如（非）对称、散列算法等，加速开发"
