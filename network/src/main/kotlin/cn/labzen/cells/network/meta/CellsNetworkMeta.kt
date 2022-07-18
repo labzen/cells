e21455449e7c807b6e6fbcf245f1d6b48f2dbfc3
Dean Zhao <rcarlosdasilva@qq.com>
2022-07-18 13:22:21 +0800

    refactor: network, 修改原spi接口

--- network/src/main/kotlin/cn/labzen/cells/network/meta/CellsNetworkMeta.kt
+++ network/src/main/kotlin/cn/labzen/cells/network/meta/CellsNetworkMeta.kt
@@ -1,8 +1,14 @@
 package cn.labzen.cells.network.meta
 
-import cn.labzen.meta.LabzenMeta
+import cn.labzen.meta.component.LabzenComponent
 
-class CellsNetworkMeta : LabzenMeta {
+class CellsNetworkMeta : LabzenComponent {
+
+  override fun mark(): String =
+    "Labzen-Cells.Network"
+
+  override fun packageBased(): String =
+    "cn.labzen.cells.network"
 
   override fun description(): String =
     "网络包，提供轻量级的Http/Tcp服务器或客户端快速开发，以及快捷的网络功能支撑"
